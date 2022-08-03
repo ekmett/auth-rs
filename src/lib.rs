2e6b8afae5166ae5e9abb7f306a66d2925b30e3b
Edward Kmett <ekmett@gmail.com>
2022-08-04 01:29:10 +0200

    auth works

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,15 @@
-#![feature(specialization)]
-
 use derivative::{self,Derivative};
 use digest::Output;
 use hex;
 use serde_json;
 use serde::ser::{Serialize, Serializer};
-use serde::de::{Deserialize, Deserializer, Visitor};
+use serde::de::{Deserialize, DeserializeOwned, Deserializer, Visitor};
 use sha1::{Digest, Sha1};
 use std::{
-  fmt::{self, Display}, 
-  marker::PhantomData, 
+  fmt::{self, Display},
+  marker::PhantomData,
   slice,
-  vec::Vec, 
+  vec::Vec,
 };
 
 // approximately [u8;20]
@@ -28,60 +26,18 @@ fn hash<A:Serialize>(value: &A) -> HashCode {
   hash_str(&serde_json::to_string(value).unwrap())
 }
 
-pub trait Evidence where
-  Self::Of : Serialize,
-{
-  type Of;
-  type Db;
-  fn auth(a:Self::Of) -> Self;
-  fn unauth(self, db: &mut Self::Db) -> Self::Of;
-}
-
-pub trait AuthEvidence
-{
-  type Of;
-  type Db;
-  fn authauth(a:Self::Of) -> Self;
-  fn unauthauth(self, db: &mut Self::Db) -> Self::Of;
-}
-
-impl <T: AuthEvidence> Evidence for T where
-  <T as AuthEvidence>::Of : Serialize {
-  type Of = <T as AuthEvidence>::Of;
-  type Db = <T as AuthEvidence>::Db;
-
-  fn auth(a:Self::Of) -> Self {
-    T::authauth(a)
-  }
-  fn unauth(self, db: &mut Self::Db) -> Self::Of {
-    T::unauthauth(self,db)
-  }
-}
-
-pub trait HasEvidence<A> where 
-  Self::Ev : Evidence<Of=A,Db=Self> { 
-    type Ev;
-}
-
-#[derive(Debug)]
-pub struct Prover(Vec<String>);
-
-impl <A: Serialize> HasEvidence<A> for Prover {
-  type Ev = Proof<A>;
-}
 
 #[derive(Debug,Copy,Clone,Derivative)]
 #[derivative(Hash,PartialEq,Eq,PartialOrd,Ord)]
-pub struct Proof<A>(
+pub struct Proof<A> {
   #[derivative(Hash="ignore",PartialEq="ignore",PartialOrd="ignore")]
-  A,
-  HashCode
-);
-
+  value: Option<A>,
+  hash:  HashCode
+}
 
 impl <A> Display for Proof<A> {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    hex::encode(self.1).fmt(f)
+    hex::encode(self.hash).fmt(f)
   }
 }
 
@@ -89,26 +45,64 @@ impl <A> Serialize for Proof<A> {
   fn serialize<S>(&self,s:S) -> Result<S::Ok, S::Error> where
     S: Serializer,
   {
-    s.serialize_str(&hex::encode(self.1))
+    s.serialize_str(&hex::encode(self.hash))
   }
 }
 
-impl <A: Serialize> Evidence for Proof<A> 
-{
-  type Of = A;
-  type Db = Prover;
-  fn auth(a:A) -> Self {
+struct ProofVisitor<A>(PhantomData<*mut A>);
+
+impl<'de,A> Visitor<'de> for ProofVisitor<A> {
+    type Value = Proof<A>;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a proof")
+    }
+
+    fn visit_str<E>(self, h: &str) -> Result<Self::Value, E> where
+      E: serde::de::Error,
+    {
+      let v = hex::decode(h).unwrap();
+      Ok(
+        Proof {
+          value: None,
+          hash: HashCode::from(<[u8;20]>::try_from(v).unwrap())
+      })
+    }
+}
+
+impl <'de,A> Deserialize<'de> for Proof<A> {
+  fn deserialize<D>(d:D) -> Result<Self, D::Error> where
+    D: Deserializer<'de> {
+    d.deserialize_str(ProofVisitor(PhantomData))
+  }
+}
+
+pub trait Db where {
+  fn auth<A : Serialize + DeserializeOwned>(&mut self, a:A) -> Proof<A>;
+  fn unauth<A : Serialize + DeserializeOwned>(&mut self, p: Proof<A>) -> A;
+}
+
+#[derive(Debug,Clone)]
+pub struct Prover { tape: Vec<String> }
+
+impl Prover {
+  pub fn new() -> Self { Prover { tape: Vec::new() } }
+  pub fn verify(&self) -> Verifier<'_> {
+    Verifier(self.tape.iter())
+  }
+}
+
+impl Db for Prover {
+  fn auth<A : Serialize + DeserializeOwned>(&mut self, a:A) -> Proof<A>{
     let h = hash(&a);
-    Proof(a,h)
+    Proof { value: Some(a), hash: h }
   }
-  fn unauth(self, db: &mut Self::Db) -> A {
-    db.0.push(serde_json::to_string(&self.0).unwrap());
-    self.0
+  fn unauth<A : Serialize + DeserializeOwned>(&mut self, p: Proof<A>) -> A {
+    let r = p.value.unwrap();
+    self.tape.push(serde_json::to_string(&r).unwrap());
+    r
   }
 }
-
-impl <T> AuthEvidence for Proof<Proof<T>> {}
-
 
 #[derive(Debug,Clone)]
 pub struct Verifier<'at>(slice::Iter<'at,String>);
@@ -120,67 +114,74 @@ impl <'at> Iterator for Verifier<'at> {
   }
 }
 
-#[derive(Debug,Copy,Clone,Derivative)]
-#[derivative(Hash,PartialEq,Eq,PartialOrd,Ord)]
-pub struct Verified<'at,A>(
-  #[derivative(Hash="ignore",PartialEq="ignore",PartialOrd="ignore")]
-  PhantomData<&'at A>, 
-  HashCode
-);
-
-impl <'at,A> Evidence for Verified<'at,A> where
-  A: Serialize + Deserialize<'at> + 'at
-{
-  type Of = A;
-  type Db = Verifier<'at>;
-
-  fn auth(a:A) -> Self {
-    Verified(PhantomData,hash(&a))
+impl <'at> Db for Verifier<'at> {
+  fn auth<A : Serialize + DeserializeOwned>(&mut self, a:A) -> Proof<A> {
+    Proof { value: None, hash: hash(&a) }
   }
-
-  fn unauth(self, db: &mut Verifier<'at>) -> A {
-    let v = db.next().unwrap();
-    assert_eq!(self.1,hash_str(&v));
+  fn unauth<A : Serialize + DeserializeOwned>(&mut self, p: Proof<A>) -> A {
+    let v = self.next().unwrap();
+    assert_eq!(p.hash,hash_str(&v));
     serde_json::from_str(&v).unwrap()
   }
 }
 
-impl <'at,A> HasEvidence<A> for Verifier<'at> where 
-  A: Serialize + Deserialize<'at> + 'at 
-{
-  type Ev = Verified<'at,A>;
-}
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use serde::{Serialize,Deserialize};
+  #[derive(Serialize,Deserialize)]
+  pub enum Tree {
+    Tip(u32),
+    Bin(u32,Box<Proof<Tree>>,Box<Proof<Tree>>)
+  }
 
-impl <'at,A> Serialize for Verified<'at,A> {
-  fn serialize<S>(&self,s:S) -> Result<S::Ok, S::Error> where
-    S: Serializer,
-  {
-    s.serialize_str(&hex::encode(self.1))
+  pub enum Dir {
+    L,
+    R
+  }
+
+  type Path = [Dir];
+
+  fn tip(u: u32) -> Tree { Tree::Tip(u) }
+  fn bin<D:Db>(db:&mut D, a: u32, l: Tree, r: Tree) -> Tree {
+    let nl = db.auth(l);
+    let nr = db.auth(r);
+    Tree::Bin(a,Box::new(nl), Box::new(nr))
+  }
+
+  fn at<D:Db>(db:&mut D,mut t: Tree, p:&Path) -> Option<u32> {
+    for ele in p {
+      if let Tree::Bin(_a,l,r) = t {
+        let nt = match ele {
+          Dir::L => l,
+          Dir::R => r
+        };
+        t = db.unauth(*nt);
+      } else {
+        None?
+      }
+    }
+    match t {
+      Tree::Bin(a,_,_) => Some(a),
+      Tree::Tip(a) => Some(a)
+    }
+  }
+
+  fn go<D:Db>(db:&mut D) -> Option<u32> {
+    let y = bin(db,0,tip(1),tip(2));
+    let x = bin(db,0,y,tip(2));
+    at(db,x,&[Dir::L,Dir::R])
+  }
+
+  #[test]
+  fn it_works() {
+    let mut p = Prover::new();
+    let result = go(&mut p);
+    println!("{result:?}");
+
+    let mut v = p.verify();
+    let result2 = go(&mut v);
+    println!("{result2:?}");
+    assert_eq!(result,result2)
   }
 }
-
-struct VerifiedVisitor<'at,A>(PhantomData<&'at A>);
-
-impl<'de,'at, A> Visitor<'de> for VerifiedVisitor<'at,A> {
-    type Value = Verified<'at,A>;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("a hashed structure")
-    }
-
-    fn visit_string<E>(self, h: String) -> Result<Self::Value, E> where
-      E: serde::de::Error,
-    {
-      let v = hex::decode(h).unwrap();
-      Ok(Verified(PhantomData,HashCode::from(<[u8;20]>::try_from(v).unwrap())))
-    }
-}
-
-impl <'at,'de,A> Deserialize<'de> for Verified<'at,A> {
-  fn deserialize<D>(d:D) -> Result<Self, D::Error> where
-    D: Deserializer<'de> {
-    d.deserialize_str(VerifiedVisitor(PhantomData))
-  }
-}
-
-
